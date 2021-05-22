c4db6cb1210bd6341e0215f5f7fbca8462ecfa73
Sergei <stagephoning@yandex.ru>
2021-05-22 22:34:10 +0300

    remove_error

--- api_yamdb/settings.py
+++ api_yamdb/settings.py
@@ -79,7 +79,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
     {
         'NAME': (
-            'django.contrib.auth.password_validation.' 
+            'django.contrib.auth.password_validation.'
             'MinimumLengthValidator'
         ),
     },
